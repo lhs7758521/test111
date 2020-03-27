7aac61d7170e773c7ac79b274d8f5bd801c8d045
lhs7758521 <1195665035@qq.com>
2020-03-27 22:33:06 +0800

    1111

--- src/com/atguigu/bean/Student.java
+++ src/com/atguigu/bean/Student.java
@@ -3,7 +3,7 @@ package com.atguigu.bean;
 public class Student {
 
 	public static void main(String[] args) {
-		System.out.println("我是刘鹤松");
+		System.out.println("我是刘鹤松1111");
 		System.out.println("我是周家平");
 	}
 }
